3cdb309de03921511759fa9e91bf5596ddb5fe7b
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-04-30 11:53:05 +0100

    fileread() needs a full url path to not give crummy corrupted files

--- php/11052011.php
+++ php/11052011.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-disposition: attachment; filename=images/flyers/11052011.png');
 header('Content-type: image/png');
-readfile('images/flyers/11052011.png');
+readfile('http://www.hearsepileup.co.uk/images/flyers/11052011.png');
 ?>
--- php/gigs.php
+++ php/gigs.php
@@ -1,5 +1,5 @@
 <h1>NEXT GIG:</h1>
-<p><strong><a href="images/flyers/11052011.jpg" rel="lightbox" title="DOA @ The Comedy 11/05/2011">HEARSE PILEUP</strong><br /> </a>
+<p><strong><a href="images/flyers/11052011.png" rel="lightbox" title="DOA @ The Comedy 11/05/2011">HEARSE PILEUP</strong><br /> </a>
 play Dead or Alive @ The Comedy,<br />
 <a href="http://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=7+Oxendon+St.,+London,+SW1Y+4EE+&amp;ie=UTF8&amp;hq=&amp;hnear=7+Oxendon+St,+London+SW1Y+4EE,+United+Kingdom&amp;z=16" target="_blank">7 Oxendon St., London, SW1Y 4EE</a> <br />
 (next door to The Comedy Store; tubes: Leicester Sq. &amp; Piccadilly Circus) <br />
