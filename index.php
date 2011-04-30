e84e6f8ba063502f5929f717b4a8e65ea6c65a1a
Samuel Carlisle <samuelcarlisle@gmail.com>
2011-04-30 02:32:15 +0100

    sam make tidy.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- 
+_-_-                                          -__ /\\     ,,                   
+  /,              _                             ||  \\  ' ||                   
+  || __    _-_   < \, ,._-_  _-_,  _-_         /||__|| \\ ||  _-_  \\ \\ -_-_  
+ ~||-  -  || \\  /-||  ||   ||_.  || \\        \||__|| || || || \\ || || || \\ 
+  ||===|| ||/   (( ||  ||    ~ || ||/           ||  |, || || ||/   || || || || 
+ ( \_, |  \\,/   \/\\  \\,  ,-_-  \\,/        _-||-_/  \\ \\ \\,/  \\/\\ ||-'  
+       `                                        ||                       |/    
+                                              Zombie rock and roll       '         											  
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>HEARSE PILUP... COMING BACK FROM THE DEAD... SOON...</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+		<meta content="Hearse Pileup; Zombie Rock and Roll" name="description" />
+		<meta content="Matt, Gaffen, Matthew, Music, QOTSA, Dead Weather, Tony Hoang, Wojciech Tybus, Birthday Party, Tone-Deaf Messiah" name="keywords" />
+		<link rel="stylesheet" type="text/css" href="css/screen.css" />
+		<link rel="shortcut icon" href="favicon.png" />
+		<!--<link rel="alternate" type="application/rss+xml" title="rss_feed_title" href="link_to_your_rss_feed" />-->
+	</head>
+	
+	<body>
+		<div id="wrapper">
+			<div id="holder">
+				<img src="images/comingback.png" alt="Coming back from the dead... Soon" />
+			</div>
+			<div id="header">
+				<div id="navbar"></div>
+			</div>
+			<div id="content">
+			</div>
+			<div id="footer">
+			</div>
+		</div>
+	</body>
+	
+</html>
