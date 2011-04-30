65ab2dc69c7ede1911d0c561e8c9cf9b9265b492
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-04-30 03:21:54 +0100

    lightbox tweak

--- script/lightbox.js
+++ script/lightbox.js
@@ -38,8 +38,8 @@
 //
 
 // If you would like to use a custom loading image or close button reference them in the next two lines.
-var loadingImage = 'loading.gif';		
-var closeButton = 'close.gif';		
+var loadingImage = 'http://www.hearsepileup.co.uk/images/loading.gif';		
+var closeButton = 'http://www.hearsepileup.co.uk/images/close.gif';		
 
 
 
