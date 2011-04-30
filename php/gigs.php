d2daa26ef9222b3a917f5b3c1530046a11a6ec35
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-04-30 04:21:25 +0100

    Also added the _blank for the gmaps link

--- php/gigs.php
+++ php/gigs.php
@@ -1,7 +1,7 @@
 <h1>NEXT GIG:</h1>
 <p><strong><a href="images/flyers/11052011.jpg" rel="lightbox" title="DOA @ The Comedy 11/05/2011">HEARSE PILEUP</strong><br /> </a>
 play Dead or Alive @ The Comedy,<br />
-<a href="http://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=7+Oxendon+St.,+London,+SW1Y+4EE+&amp;ie=UTF8&amp;hq=&amp;hnear=7+Oxendon+St,+London+SW1Y+4EE,+United+Kingdom&amp;z=16">7 Oxendon St., London, SW1Y 4EE</a> <br />
+<a href="http://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=7+Oxendon+St.,+London,+SW1Y+4EE+&amp;ie=UTF8&amp;hq=&amp;hnear=7+Oxendon+St,+London+SW1Y+4EE,+United+Kingdom&amp;z=16" target="_blank">7 Oxendon St., London, SW1Y 4EE</a> <br />
 (next door to The Comedy Store; tubes: Leicester Sq. &amp; Piccadilly Circus) <br />
 Wednesday 11th May; 8.00pm</p>
 <p><a href="11052011.php" title="DOA @ The Comedy 11/05/2011">&pound;5 entry with this flyer</a>
