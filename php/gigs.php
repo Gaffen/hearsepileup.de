a6e7c10d9f0d095c20c34b29b4c52ec1c31409a4
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-04-30 03:25:08 +0100

    lightbox tweak

--- php/gigs.php
+++ php/gigs.php
@@ -1,5 +1,5 @@
 <h1>NEXT GIG:</h1>
-<p><strong><a href="http:/www,hearsepileup.co.uk/images/flyers/11052011.jpg" rel="lightbox" title="DOA @ The Comedy 11/05/2011">HEARSE PILEUP</strong><br /> 
+<p><strong><a href="http:/www.hearsepileup.co.uk/images/flyers/11052011.jpg" rel="lightbox" title="DOA @ The Comedy 11/05/2011">HEARSE PILEUP</strong><br /> 
 play Dead or Alive @ The Comedy,</a><br />
 7 Oxendon St., London, SW1Y 4EE <br />
 (next door to The Comedy Store; tubes: Leicester Sq. & Piccadilly Circus) <br />
