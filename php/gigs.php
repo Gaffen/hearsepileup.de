fb0ba0498ed3f74c51dbd1e8b588facf64f89275
Gaffen <tonedeafmessiah@gmail.com>
2011-05-13 12:09:35 +0100

    Spelling mistake corrected D:

--- php/gigs.php
+++ php/gigs.php
@@ -1,6 +1,6 @@
 <h1>NEXT GIG:</h1>
 <p><strong><a href="images/flyers/22052011.png" rel="lightbox" title="SE13 RECORDS @ The HOB 22/05/2011">HEARSE PILEUP</strong><br /> </a>
-SE13 Records @ The Pub,<br />
+Play SE13 Records @ The Hob,<br />
 <a href="http://maps.google.co.uk/maps?ie=UTF8&q=the+hob+forest+hill&fb=1&gl=uk&hq=the+hob+forest+hill&hnear=0x48761b5dedeb3be5:0x54f085cb18ec65c9,Islington&hl=en&view=map&cid=2754844324650035941&z=16&iwloc=A" target="_blank">7 Devonshire Road, London, SE23 3HE</a> <br />
 (Opposite Forest Hill Rail Station) <br />
 Sunday 22th May; 4.00pm</p>
