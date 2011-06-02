2cc72049c745df9169b4a204205533cca9854bbc
Gaffen <tonedeafmessiah@gmail.com>
2011-06-02 15:48:41 +0100

    news section updated

--- php/gigs.php
+++ php/gigs.php
@@ -1,7 +1,7 @@
 <div id="gigs">
 <h1>NEXT GIG: Coming Soon...</h1>
 <p><strong><a href="images/flyers/22052011.png" rel="lightbox" title="SE13 RECORDS @ The HOB 22/05/2011">HEARSE PILEUP</strong><br /><br /> </a>
-are currently getting ready for their first recording session, more gigs coming soon</p><br />
+are currently getting ready for their first recording session, more gigs coming soon</p>
 <p>Stay informed by following <a href="http://www.mattsdeadcat.blogspot.com">Matt's blog.</a></p>
 <p>See you soon. Remember to make <a href="https://github.com/Gaffen/hearsepileup.co.uk" target="_blank">pull requests on git</a> for added functionality on the site</p>
 <!-- <a href="http://maps.google.co.uk/maps?ie=UTF8&q=the+hob+forest+hill&fb=1&gl=uk&hq=the+hob+forest+hill&hnear=0x48761b5dedeb3be5:0x54f085cb18ec65c9,Islington&hl=en&view=map&cid=2754844324650035941&z=16&iwloc=A" target="_blank">7 Devonshire Road, London, SE23 3HE</a> <br />
