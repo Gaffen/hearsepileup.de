b31136a9b6d02a65f751ad86dc30b20d61d155c2
Gaffen <tonedeafmessiah@gmail.com>
2011-07-03 20:51:31 +0100

    new pictures, gig, etc.

---
+++ ad/ad/php/22052011.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-disposition: attachment; filename=images/flyers/22052011.png');
+header('Content-type: image/png');
+readfile('http://www.hearsepileup.co.uk/images/flyers/22052011.png');
+?>
---
+++ ad/ad/php/gigs.php
@@ -0,0 +1,11 @@
+<div id="gigs">
+<h1>NEXT GIG: Coming Soon...</h1>
+<p><strong><a href="images/flyers/22052011.png" rel="lightbox" title="SE13 RECORDS @ The HOB 22/05/2011">HEARSE PILEUP</strong><br /><br /> </a>
+are currently getting ready for their first recording session, more gigs coming soon</p>
+<p>Stay informed by following <a href="http://www.mattsdeadcat.blogspot.com">Matt's blog.</a></p>
+<p>See you soon. Remember to make <a href="https://github.com/Gaffen/hearsepileup.co.uk" target="_blank">pull requests on git</a> for added functionality on the site</p>
+<!-- <a href="http://maps.google.co.uk/maps?ie=UTF8&q=the+hob+forest+hill&fb=1&gl=uk&hq=the+hob+forest+hill&hnear=0x48761b5dedeb3be5:0x54f085cb18ec65c9,Islington&hl=en&view=map&cid=2754844324650035941&z=16&iwloc=A" target="_blank">7 Devonshire Road, London, SE23 3HE</a> <br />
+(Opposite Forest Hill Rail Station) <br />
+Sunday 22th May; 4.00pm</p>
+<p><a href="php/22052011.php" title="DOA @ The Comedy 11/05/2011">&pound;2 entry with this flyer</a> -->
+</div>
