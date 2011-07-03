601c3598670c8f0aeede79e6af48fe05e88b212f
Gaffen <tonedeafmessiah@gmail.com>
2011-07-03 21:17:36 +0100

    gig added

--- ad/ad/php/gigs.php
+++ ad/ad/php/gigs.php
@@ -1,11 +1,9 @@
 <div id="gigs">
-<h1>NEXT GIG: Coming Soon...</h1>
-<p><strong><a href="images/flyers/22052011.png" rel="lightbox" title="SE13 RECORDS @ The HOB 22/05/2011">HEARSE PILEUP</strong><br /><br /> </a>
-are currently getting ready for their first recording session, more gigs coming soon</p>
-<p>Stay informed by following <a href="http://www.mattsdeadcat.blogspot.com">Matt's blog.</a></p>
-<p>See you soon. Remember to make <a href="https://github.com/Gaffen/hearsepileup.co.uk" target="_blank">pull requests on git</a> for added functionality on the site</p>
-<!-- <a href="http://maps.google.co.uk/maps?ie=UTF8&q=the+hob+forest+hill&fb=1&gl=uk&hq=the+hob+forest+hill&hnear=0x48761b5dedeb3be5:0x54f085cb18ec65c9,Islington&hl=en&view=map&cid=2754844324650035941&z=16&iwloc=A" target="_blank">7 Devonshire Road, London, SE23 3HE</a> <br />
-(Opposite Forest Hill Rail Station) <br />
-Sunday 22th May; 4.00pm</p>
-<p><a href="php/22052011.php" title="DOA @ The Comedy 11/05/2011">&pound;2 entry with this flyer</a> -->
+<h1>NEXT GIG:</h1>
+<p><strong><a href="images/HearsePileup.png" rel="lightbox" title="Hearse Pileup">HEARSE PILEUP PLAY THE ARC</strong></a>
+<a href="http://maps.google.co.uk/maps?q=Torrens+Street,+City+of+London+EC1V+1NQ&ll=51.532135,-0.105443&spn=0.004699,0.00912&gl=uk&z=17" target="_blank">7 Torrens Street, City of London EC1V 1NQ</a> <br />
+(Round the corner from Angel Tube) <br />
+Friday 25th July; 8.00pm</p>
+<p>
+    <!--<a href="php/22052011.php" title="DOA @ The Comedy 11/05/2011">&pound;2 entry with this flyer</a>-->
 </div>
