36b743c17cc6eaddba5be0b57a8e62bf0e9f342a
Gaffen <tonedeafmessiah@gmail.com>
2011-07-04 10:03:23 +0100

    entry to the next gig is free

--- ad/ad/php/gigs.php
+++ ad/ad/php/gigs.php
@@ -3,6 +3,7 @@
 <p><strong><a href="images/HearsePileup.png" rel="lightbox" title="Hearse Pileup">HEARSE PILEUP PLAY THE ARC</strong></a></p>
 <p><a href="http://maps.google.co.uk/maps?q=Torrens+Street,+City+of+London+EC1V+1NQ&ll=51.532135,-0.105443&spn=0.004699,0.00912&gl=uk&z=17" target="_blank">7 Torrens Street, City of London EC1V 1NQ</a> <br />
 (Round the corner from Angel Tube) <br />
+<h2>FREE ENTRY</h2>
 Friday 25th July; 8.00pm</p>
     <!--<a href="php/22052011.php" title="DOA @ The Comedy 11/05/2011">&pound;2 entry with this flyer</a>-->
 </div>
