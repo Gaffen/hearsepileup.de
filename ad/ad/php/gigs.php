af12fca4ad9496948006ab7161fafacafadf3534
Gaffen <tonedeafmessiah@gmail.com>
2011-07-04 11:32:51 +0100

    date correction

--- ad/ad/php/gigs.php
+++ ad/ad/php/gigs.php
@@ -4,6 +4,6 @@
 <p><a href="http://maps.google.co.uk/maps?q=Torrens+Street,+City+of+London+EC1V+1NQ&ll=51.532135,-0.105443&spn=0.004699,0.00912&gl=uk&z=17" target="_blank">7 Torrens Street, City of London EC1V 1NQ</a> <br />
 (Round the corner from Angel Tube) <br />
 <h2>FREE ENTRY</h2>
-Friday 25th July; 8.00pm</p>
+Friday 15th July; 8.00pm</p>
     <!--<a href="php/22052011.php" title="DOA @ The Comedy 11/05/2011">&pound;2 entry with this flyer</a>-->
 </div>
