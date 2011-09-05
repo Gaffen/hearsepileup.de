94d9532a61204161651dba1746ca02697cf920eb
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-09-05 20:55:04 +0100

    New Gig

--- ad/ad/php/10092011.php
+++ ad/ad/php/10092011.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-disposition: attachment; filename=images/flyers/10092011.png');
-header('Content-type: image/png');
-readfile('http://www.hearsepileup.co.uk/images/flyers/10092011.png');
+header('Content-disposition: attachment; filename=images/flyers/10092011.jpg');
+header('Content-type: image/jpg');
+readfile('http://www.hearsepileup.co.uk/images/flyers/10092011.jpg');
 ?>
