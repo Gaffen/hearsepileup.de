842a5588acbfa626d89eda031aa58b84111090ed
Matthew Gaffen <tonedeafmessiah@gmail.com>
2011-10-28 02:51:37 +0100

    major new redesign

--- ad/ad/index.php
+++ ad/ad/index.php
@@ -1,53 +1,89 @@
 <!-- 
-_-_-                                          -__ /\\     ,,                   
-  /,              _                             ||  \\  ' ||                   
+_-_-                                          -__ /\\     ,,
+  /,              _                             ||  \\  ' ||
   || __    _-_   < \, ,._-_  _-_,  _-_         /||__|| \\ ||  _-_  \\ \\ -_-_  
  ~||-  -  || \\  /-||  ||   ||_.  || \\        \||__|| || || || \\ || || || \\ 
   ||===|| ||/   (( ||  ||    ~ || ||/           ||  |, || || ||/   || || || || 
  ( \_, |  \\,/   \/\\  \\,  ,-_-  \\,/        _-||-_/  \\ \\ \\,/  \\/\\ ||-'  
        `                                        ||                       |/    
-                                              Zombie rock and roll       '         											  
+                                              Zombie rock and roll       '
 -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>HEARSE PILEUP... COMING BACK FROM THE DEAD... SOON...</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-		<meta content="Hearse Pileup; Zombie Rock and Roll" name="description" />
-		<meta content="Matt, Gaffen, Matthew, Music, QOTSA, Dead Weather, Tony Hoang, Wojciech Tybus, Birthday Party, Tone-Deaf Messiah" name="keywords" />
-		<link rel="stylesheet" type="text/css" href="css/screen.css" />
-		<link rel="shortcut icon" href="favicon.png" />
-		<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
-		<script type="text/javascript" src="script/lightbox.js"></script>
-		<!--<link rel="alternate" type="application/rss+xml" title="rss_feed_title" href="link_to_your_rss_feed" />-->
-		<script type="text/javascript">
 
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-9918791-5']);
-		  _gaq.push(['_trackPageview']);
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
 
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		</script>
-	</head>
-	
-	<body>
-		<div id="wrapper">
-			<img id="holder" src="images/comingback.png" alt="Coming back from the dead... Soon" border="0" />
-			<div id="header">
-				<div id="navbar"></div>
-			</div>
-			<div id="content"><?php include("php/gigs.php"); ?>
-			</div>
-			<div id="footer">
-			<p><a href="http://www.facebook.com/Hearse.Pileup" target="_blank">www.facebook.com/Hearse.Pileup</a><p>
-			<p class="ninja">Hearsepileup.co.uk is an open source website, with its code released on <a href="https://github.com/Gaffen/hearsepileup.co.uk" target="_blank">github.com</p></a>
-			</div>
-		</div>
-	</body>
-	
+    <title>HEARSE PILEUP</title>
+	<meta content="Hearse Pileup; Zombie Rock and Roll" name="description" />
+	<meta content="Matt, Gaffen, Matthew, Music, QOTSA, Dead Weather, Tony Hoang, Wojciech Tybus, Birthday Party, Tone-Deaf Messiah" name="keywords" />
+	<link rel="stylesheet" type="text/css" href="css/HearseLooks.css" />
+	<link rel="stylesheet" type="text/css" href="css/lightbox.css" />
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<link rel="shortcut icon" href="favicon.png" />
+  
+    <script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <div id="sky">&nbsp
+            </div>
+            <div id="logo">&nbsp
+            </div>
+        </header>
+        <div id="obituaries">
+            <a href="http://twitter.com/#!/HearsePileup"><img src="images/HearsePileupTwitter.png" alt="Squark Squark" /></a>
+            <a href="http://www.facebook.com/Hearse.Pileup"><img src="images/HearsePileupFacebook.png" alt="'Like' us, but surely you already do?!'" /></a>
+            <a href="http://soundcloud.com/hearse-pileup"><img src="images/HearsePileupSoundcloud.png" alt="What you came here for; the music" /></a>
+            <a href="http://www.youtube.com/user/tonedeafmessiah"><img src="images/HearsePileupYoutube.png" alt="Watch us, for whatever reasons you might have..." /></a>
+        </div>
+        <div id="content" role="main">
+        <iframe width="460" height="264" src="http://www.youtube.com/embed/7JmL0z-bJR0?rel=0" frameborder="0" allowfullscreen></iframe>
+        <div id="gigs">
+        <h1>Hearse Pileup's next gig:</h1>
+        <p>The Hearse Pileup website is currently being refurbished, it will take a little longer gefore our gig linstings are available once again.</p>
+        <p>In the mean time we can announce that our next gig is at the Macbeth in Shoreditch on Wednesday the 16th November. £7 entry, £5 guestlist. Reserve guestlist places <a href="http://www.facebook.com/Hearse.Pileup">on our facebook page</a>.</p>
+        </div>
+			<h2 class="ninja">Hearsepileup.co.uk is an open source website, with its code released on <a href="https://github.com/Gaffen/hearsepileup.co.uk" target="_blank">github.com</h2></a>
+        </div>
+    </div>
+
+    <!-- JavaScript at the bottom for fast page loading -->
+
+    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.4.min.js"><\/script>')</script>
+	<script type="text/javascript" src="js/lightbox.js"></script>
+
+    <!-- scripts concatenated and minified via build script -->
+    <script defer src="js/plugins.js"></script>
+    <script defer src="js/script.js"></script>
+    <!-- end scripts -->
+
+    <script>
+    var _gaq=[['_setAccount','UA-9918791-5'],['_trackPageview'],['_trackPageLoadTime']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+
+</body>
 </html>
