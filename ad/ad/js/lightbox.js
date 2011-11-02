f8da72c1f5bedf822aaa2394b8b788854300b1ea
Gaffen <tonedeafmessiah@gmail.com>
2011-11-02 17:59:21 +0000

    lightbox elements

--- ad/ad/js/lightbox.js
+++ ad/ad/js/lightbox.js
@@ -35,8 +35,8 @@
 //
 
 // If you would like to use a custom loading image or close button reference them in the next two lines.
-var loadingImage = 'loading.gif';		
-var closeButton = 'close.gif';		
+var loadingImage = 'http://www.hearsepileup.de/ad/images/loading.gif';		
+var closeButton = 'http://www.hearsepileup.de/ad/images/close.gif';		
 
 
 
