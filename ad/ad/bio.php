76c3068a4b648f6f4b404b096e7c1f655d91d826
Vixxenne Ellis-Dee <vxn89@Vixxennes-MacBook-Pro.local>
2012-05-03 21:21:26 +1000

    Bio page added

--- ad/ad/bio.php
+++ ad/ad/bio.php
@@ -1,15 +1,11 @@
 <?php include("php/header.php"); ?>
 	    <script type="text/javascript">
-		//$('#content').addClass('gaffangry');
+		$('#content').addClass('biobg');
 	    </script>
 	    <h1>Bio</h1>
-		<div class="twocol"><h2>We've all been raised on television to believe that one day we'd all be millionaires, and movie gods, and rock stars. But we won't. And we're slowly learning that fact. And we're very, very pissed off.</h2></div>
-		<div class="twocol"><p>Hearse Pileup is the sound of rock 'n' roll for the undead. As the earth becomes a ticking timebomb of war, cataclysmic environmental disaster and gross over consumption there is only one message to our music</p>
-		</div>
-		<div class="onecol"><h3>SOMETHING IS VERY, VERY WRONG</h3>
-		</div>
-		<div class="twocol"><p>Spitting fire and pissing vinegar, Hearse Pileup are part nostalgic throwback to 70's and 80's stoner and punk rock and part anarchic doomsayers. Heavily influenced by the likes of Motorhead, Queens of the Stone Age, Iggy pop and Black Sabbath, Hearse Pileup bring balls-to-the-wall hard Rock 'n' Roll back into the 21st century</p></div>
-		<div class="twocol"><p>Hearse Pileup have had many London-based gigs and have just finished recording their soon to be released EP at Dropout studio. They have just released a short free-to-download live album on bandcamp, and for the most recent news you can subscribe to their newsletter.</p>
-		</div>
+		<div class="twocol"><p>Hearse Pileup is the sound of rock 'n' roll for the undead. As the earth becomes a ticking timebomb of war, cataclysmic environmental disaster and gross over consumption there is only one message to our music:</p>
+<p><strong>SOMETHING IS VERY, VERY WRONG</strong></p>
+<p>Spitting fire and pissing vinegar, Hearse Pileup are part nostalgic throwback to 70's/80's stoner/punk rock and part anarchic doomsayers. Heavily influenced by the likes of Motorhead, Queens of the Stone Age, Iggy pop and Black Sabbath, Hearse Pileup bring balls-to-the-wall hard Rock 'n' Roll back into the 21st century.</p>
+<p>Hearse Pileup have had many London-based gigs and have just finished recording their soon to be released EP at Dropout studio. They have just released a short free-to-download live album on bandcamp, and for the most recent news you can subscribe to their newsletter.</p></div>
 	</div>
 <?php include("php/footer.php"); ?>
