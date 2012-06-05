6223800316721d07ee7a17cc6ecf8c9ca740fd0c
Vixxenne Ellis-Dee <vxn89@Vixxennes-MacBook-Pro.local>
2012-06-06 06:22:57 +1000

    Contact page added

---
+++ ad/ad/contact.php
@@ -0,0 +1,10 @@
+<?php include("php/header.php"); ?>
+	    <script type="text/javascript">
+		$('#content').addClass('contactbg');
+	    </script>
+	    <h1>Contact</h1>
+		<div class="logocol"><p>Email:<span><a title="Email" href="mailto:hearse.pileup@gmail.com">hearse.pileup@gmail.com</a></span></p>
+<p>Twitter:<span><a title="Twitter" href="https://twitter.com/#!/hearsepileup">@HearsePileup</a></span></p>
+<p>Facebook:<span><a title="Facebook" href="http://www.facebook.com/Hearse.Pileup">facebook.com/Hearse.Pileup</a></span></p></div>
+	</div>
+<?php include("php/footer.php"); ?>
