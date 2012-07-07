bf1d846bda00b108c519585a9ceebc0d0c6576a9
Vixxenne Ellis-Dee <vxn89@Vixxennes-MacBook-Pro.local>
2012-07-08 05:13:16 +1000

    open links in new window

--- ad/ad/gigs.php
+++ ad/ad/gigs.php
@@ -13,33 +13,33 @@
     if(data["resultsPage"]["totalEntries"] === 0){
 	$("#events").append('<p><strong>No new Hearse gigs for now</strong> <br/>we must be busy getting drunk,<br/>come back soon for more gigs!<p>');
     }
-    $.each(data["resultsPage"]["results"]["event"], function(i, entry){
-	var doors = ""
-	var artists = ""
-	var prettyDate = data["resultsPage"]["results"]["event"][i]["displayName"].match(/\(.*\)/).toString();
-	prettyDate = prettyDate.substring(1, prettyDate.length - 7);
-	if(data["resultsPage"]["results"]["event"][i]["start"]["time"] != null){
-	    doors="<dt class='doortime'>Doors:</dt><dd class='doortime'>" + data["resultsPage"]["results"]["event"][i]["start"]["time"] + "</dd>"
-	}
-	if(data["resultsPage"]["results"]["event"][i]["performance"].length != 1){
-	    artists = "<dt>Also Featuring:</dt>"
-	    $.each(data["resultsPage"]["results"]["event"][i]["performance"], function(a, artist){
-		if(data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["displayName"] != "Hearse Pileup"){
-		    artists += '<dd><a href="' + data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["uri"] + '">' + data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["displayName"] + '</a></dd>'
-		}
-	    })
-	}
-	if(artists === ""){
-	    artists += "<dt>No further information has been provided at this time<br/><br/>If you would like to know more, please contact us via Facebook, Twitter or E-mail</dt>"
-	}
-	
-	$("#events").append('<li><h2><a href="'+ data["resultsPage"]["results"]["event"][i]["uri"] + '"><span>'
-			    + prettyDate + '</span> &#64; ' + data["resultsPage"]["results"]["event"][i]["venue"]["displayName"]
-			    + '</a></h2><dl class="eventdetail">' + doors
-			    + artists + '</dl><div class="clr"></div></li>');
+	$.each(data["resultsPage"]["results"]["event"], function(i, entry){
+	    var doors = ""
+	    var artists = ""
+	    var prettyDate = data["resultsPage"]["results"]["event"][i]["displayName"].match(/\(.*\)/).toString();
+	    prettyDate = prettyDate.substring(1, prettyDate.length - 7);
+	    if(data["resultsPage"]["results"]["event"][i]["start"]["time"] != null){
+		doors="<dt class='doortime'>Doors:</dt><dd class='doortime'>" + data["resultsPage"]["results"]["event"][i]["start"]["time"] + "</dd>"
+	    }
+	    if(data["resultsPage"]["results"]["event"][i]["performance"].length != 1){
+		artists = "<dt>Also Featuring:</dt>"
+		$.each(data["resultsPage"]["results"]["event"][i]["performance"], function(a, artist){
+		    if(data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["displayName"] != "Hearse Pileup"){
+			artists += '<dd><a href="' + data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["uri"] + '" target="_blank">' + data["resultsPage"]["results"]["event"][i]["performance"][a]["artist"]["displayName"] + '</a></dd>'
+		    }
+		})
+	    }
+	    if(artists === ""){
+		artists += "<dt>No further information has been provided at this time<br/><br/>If you would like to know more, please contact us via Facebook, Twitter or E-mail</dt>"
+	    }
+	    
+	    $("#events").append('<li><h2><a href="'+ data["resultsPage"]["results"]["event"][i]["uri"] + '" target="_blank"><span>'
+				+ prettyDate + '</span> &#64; ' + data["resultsPage"]["results"]["event"][i]["venue"]["displayName"]
+				+ '</a></h2><dl class="eventdetail">' + doors
+				+ artists + '</dl><div class="clr"></div></li>');
 	})   
-  }
-  });
+	}
+    });
   });
 	    </script>
 	    <ul id="events" class="events"></ul>
