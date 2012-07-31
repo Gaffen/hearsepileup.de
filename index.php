169601b3f7038daf9f8b9035d53e5fcad2fbfff1
Gaffen <tonedeafmessiah@gmail.com>
2012-07-31 23:55:12 +0100

    redirect

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE>
+    <head>
+        
+    </head>
+    <body style="background: #000;">
+        <a href="ad/"><img src="EP_Artwork.jpg" style="margin: auto; margin-top: 100px;"/></a>
+    </body>
