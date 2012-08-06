5045f626fc02f8bcc4f7718a0b8b3c78d710df43
Gaffen <tonedeafmessiah@gmail.com>
2012-08-06 17:38:17 +0100

    EP release date updated

--- ad/index.php
+++ ad/index.php
@@ -4,10 +4,10 @@
 	    </script>
 	    </script>
 	    <div id="feature">
-		<h4>COMING AUGUST 24TH</h4>
+		<h4>COMING SEPTEMBER 24TH</h4>
 		<h3>PRETTY <br/>SHINY <br/>THINGS</h3>
 		<div class="info">
-		    <h5>The single</h5>
+		    <h5>The EP</h5>
 		    <p>Sign up for the Hearse Pileup</p>
 		    <p>newsletter for updates</p>
 		</div>
